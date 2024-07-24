b0a4d31ef7363453c23796eb2e42cc4f084b4919
TimWong <timwong.tw93@gmail.com>
2024-07-24 20:33:23 +1000

    added error. home and portfolio pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,33 @@
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import './scss/styles.scss';
 import * as bootstrap from 'bootstrap';
 import './index.css';
 
 import App from './App.jsx';
+import Error from './pages/Error.jsx';
+import Home from './pages/Home.jsx';
+import Portfolio from './pages/Portfolio.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'portfolio',
+        element: <Portfolio />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
+  <RouterProvider router = {router} />
 );
