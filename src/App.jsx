66c20edc2a5b99237cdea5dc688ab825aefb1f5a
TimWong <timwong.tw93@gmail.com>
2024-07-24 20:53:23 +1000

    added a navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom';
-
+import Nav from './components/Nav';
 import './App.css';
 
 function App() {
 
   return (
     <>
-      <main>
+      <Nav />
+      <main className="mx-3">
         <Outlet />
       </main>
     </>
