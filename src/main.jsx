c781e8b8491419d0a0ae9d482ec67f9d6b2eebe3
TimWong <timwong.tw93@gmail.com>
2024-07-24 20:27:27 +1000

    added routing to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+
+import './scss/styles.scss';
+import * as bootstrap from 'bootstrap';
+import './index.css';
+
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+);
